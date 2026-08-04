8a353432ac58278c1c3b2263df974e7a0669bba8
jeeyyxs <riodwimulyono0512@gmail.com>
2026-08-04 10:21:11 +0700

    program utama

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -17,3 +17,16 @@ public:
     pelajar() { nilai = 100; }
     friend void manusia :: showNilaiPelajar(pelajar &x);
 };
+
+void manusia :: showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
